0a4b782e1da1be94036bacd73aeb770775d2edad
pranayy1 <pranayy7024@gmail.com>
2025-08-29 19:01:08 +0530

    Add all project files

--- script/main.js
+++ script/main.js
@@ -39,15 +39,15 @@ document.addEventListener("DOMContentLoaded", function () {
     "December",
   ];
 
-  const targetDay = 7;
-  const targetMonthIndex = 11; // December
-  const targetYear = 2024;
-  const birthYear = 2001;
+  const targetDay = 30;
+  const targetMonthIndex = 8; // December
+  const targetYear = 2025;
+  const birthYear = 2025;
 
   let currentDay = 1;
   let currentMonthIndex = 0;
   let currentYear = birthYear;
-  let currentAge = 0;
+  let currentAge = 20;
 
   const interval = setInterval(() => {
     // Update day, month, year, and age in the DOM
